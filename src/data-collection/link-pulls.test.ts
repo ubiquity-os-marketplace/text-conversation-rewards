310fd7c129fed00ae1246c5707a3a2742069b43d
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-19 02:41:01 +0900

    test: cross repo link pr works

--- src/data-collection/link-pulls.test.ts
+++ src/data-collection/link-pulls.test.ts
@@ -4,8 +4,8 @@ import linkPulls from "./link-pulls";
 
 // import issueNoLink from "./fixtures/issue-92.json"; // no link
 
-// import prCrossRepo from "./fixtures/pr-188.json";
-// import issueCrossRepoLink from "./fixtures/issue-89.json"; // pr188 is linked to this issue
+import issueCrossRepoLink from "./fixtures/issue-89.json"; // pr188 is linked to this issue
+import prCrossRepo from "./fixtures/pr-188.json";
 
 import issueSameRepoLink from "./fixtures/issue-90.json"; // pr91 is linked to this issue
 import prSameRepo from "./fixtures/pr-91.json";
@@ -19,7 +19,7 @@ if (!GITHUB_TOKEN) {
 process.argv = ["path/to/node", "path/to/script", `--auth=${GITHUB_TOKEN}`];
 
 // const issueNoLinkParams = parseGitHubUrl(issueNoLink.html_url); // no link
-// const issueCrossRepoLinkParams = parseGitHubUrl(issueCrossRepoLink.html_url); // cross repo link
+const issueCrossRepoLinkParams = parseGitHubUrl(issueCrossRepoLink.html_url); // cross repo link
 const issueSameRepoLinkParams = parseGitHubUrl(issueSameRepoLink.html_url); // same repo link
 
 // const prCrossRepoLink = parseGitHubUrl(prCrossRepo.html_url);
@@ -33,16 +33,13 @@ describe("linkPulls", () => {
 
   it("should find the linked pull request in the current repository", async () => {
     const result = await linkPulls(issueSameRepoLinkParams);
-    const expected = {
-      issue: {
-        node_id: prSameRepo.node_id,
-      },
-    };
+    const expected = { issue: { node_id: prSameRepo.node_id } };
     expect(result).toMatchObject(expected);
   });
 
-  // it("should search across other repositories if linked pull request is not found in the current repository", async () => {
-  //   const result = await linkPulls(issueCrossRepoLink);
-  //   expect(result).toEqual(pr188);
-  // });
+  it("should search across other repositories if linked pull request is not found in the current repository", async () => {
+    const result = await linkPulls(issueCrossRepoLinkParams);
+    const expected = { issue: { node_id: prCrossRepo.node_id } };
+    expect(result).toMatchObject(expected);
+  });
 });
