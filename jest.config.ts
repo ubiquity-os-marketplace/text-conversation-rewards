b18ed583db65a9fb2bdae9e8637cf80e8b26c8fa
Fernand <FernandVEYRIER@users.noreply.github.com>
2024-03-21 15:25:04 +0900

    chore: jest coverage and env openai key

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,11 @@ const cfg: Config = {
     "^.+\\.tsx?$": "babel-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coveragePathIgnorePatterns: ["node_modules", "mocks"],
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "clover", "json-summary"],
+  reporters: ["default", "jest-junit"],
+  coverageDirectory: "coverage",
 };
 
 export default cfg;
