a85e01c1c3ba49b3239a6622e1634eceb4e96eab
Fernand <FernandVEYRIER@users.noreply.github.com>
2024-03-21 17:27:32 +0900

    chore: removed comments in Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,4 @@
-// import { config } from "dotenv";
 import type { Config } from "jest";
-
-// config();
-// const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-// process.argv = ["path/to/node", "path/to/script", `--auth`, `${GITHUB_TOKEN}`, "--open-ai", `${OPENAI_API_KEY}`];
 
 const cfg: Config = {
   transform: {
