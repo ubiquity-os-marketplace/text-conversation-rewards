bf32d47c2680f16b7891024dfee26a09f10835eb
gentlementlegen <FernandVEYRIER@users.noreply.github.com>
2024-03-23 13:03:00 +0900

    chore: rewrote average evaluation to use full batch of comments instead of individual eval

--- src/parser/content-evaluator-module.ts
+++ src/parser/content-evaluator-module.ts
@@ -1,9 +1,10 @@
 import Decimal from "decimal.js";
+import { encodingForModel } from "js-tiktoken";
 import OpenAI from "openai";
 import configuration from "../configuration/config-reader";
 import { GetActivity } from "../get-activity";
 import { GitHubIssue } from "../github-types";
-import { Result, Module, GithubComment } from "./processor";
+import { GithubComment, Module, Result } from "./processor";
 
 /**
  * Evaluates and rates comments.
@@ -22,15 +23,16 @@ export class ContentEvaluatorModule implements Module {
     for (const key of Object.keys(result)) {
       const currentElement = result[key];
       const comments = currentElement.comments || [];
+      const specificationBody = (data.self as GitHubIssue)?.body;
 
-      for (let i = 0; i < comments.length; i++) {
-        const comment = comments[i];
-        const specificationBody = (data.self as GitHubIssue)?.body;
-        const commentBody = comment.content;
-
-        if (specificationBody && commentBody) {
-          promises.push(this._processComment(comment, specificationBody, commentBody));
-        }
+      if (specificationBody && comments.length) {
+        promises.push(
+          this._processComment(
+            comments,
+            specificationBody,
+            comments.map((comment) => comment.content)
+          )
+        );
       }
     }
 
@@ -38,23 +40,27 @@ export class ContentEvaluatorModule implements Module {
     return result;
   }
 
-  async _processComment(comment: GithubComment, specificationBody: string, commentBody: string): Promise<void> {
-    const relevance = await this._sampleRelevanceScoreResults(specificationBody, commentBody);
-    const currentReward = new Decimal(comment.score?.reward ? comment.score.reward : 0);
-    const reward = currentReward.mul(relevance).toNumber();
+  async _processComment(comments: GithubComment[], specificationBody: string, commentsBody: string[]) {
+    const relevance = await this._sampleRelevanceScoreResults(specificationBody, commentsBody);
 
-    comment.score = {
-      ...(comment.score || {}),
-      relevance: relevance.toNumber(),
-      reward,
-    };
+    for (let i = 0; i < relevance.length; i++) {
+      const currentComment = comments[i];
+      const currentRelevance = relevance[i];
+      const currentReward = new Decimal(currentComment.score?.reward ? currentComment.score.reward : 0);
+      currentComment.score = {
+        ...(currentComment.score || {}),
+        relevance: currentRelevance.toNumber(),
+        reward: currentReward.mul(currentRelevance).toNumber(),
+      };
+    }
   }
 
-  async _evaluateComment(specification: string, comment: string) {
-    const prompt = this._generatePrompt(specification, comment);
+  async _evaluateComments(specification: string, comments: string[]) {
+    const prompt = this._generatePrompt(specification, comments);
+
     try {
       const response: OpenAI.Chat.ChatCompletion = await this._openAi.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: this._getOptimalModel(prompt),
         messages: [
           {
             role: "system",
@@ -68,35 +74,63 @@ export class ContentEvaluatorModule implements Module {
         presence_penalty: 0,
       });
 
-      return { relevance: new Decimal(Number(response.choices[0].message.content)) };
+      const rawResponse = String(response.choices[0].message.content);
+      const parsedResponse = JSON.parse(rawResponse) as number[];
+      return parsedResponse.map((o) => new Decimal(o));
     } catch (error) {
-      console.error("Failed to evaluate comment", error);
-      return {
-        relevance: new Decimal(0),
-      };
+      console.error(`Failed to evaluate comment`, error);
+      return [];
     }
   }
 
-  async _sampleRelevanceScoreResults(specification: string, comment: string) {
+  _getOptimalModel(prompt: string) {
+    const encoder = encodingForModel("gpt-3.5-turbo");
+    const totalSumOfTokens = encoder.encode(prompt).length;
+
+    if (totalSumOfTokens <= 4097) {
+      return "gpt-3.5-turbo";
+    } else if (totalSumOfTokens <= 16385) {
+      return "gpt-3.5-turbo-16k";
+    } else {
+      console.warn("Backup plan for development purposes only, but using gpt-4 due to huge context size");
+      return "gpt-4";
+    }
+  }
+
+  async _sampleRelevanceScoreResults(specification: string, comments: string[]) {
     const BATCH_SIZE = 10;
-    let averageScore = new Decimal(0);
-    const evaluationPromises: ReturnType<typeof this._evaluateComment>[] = [];
+    const evaluationPromises: ReturnType<typeof this._evaluateComments>[] = [];
 
     for (let i = 0; i < BATCH_SIZE; ++i) {
-      evaluationPromises.push(this._evaluateComment(specification, comment));
+      evaluationPromises.push(this._evaluateComments(specification, comments));
     }
-    const results = await Promise.all(evaluationPromises);
-    results.forEach((result) => {
-      averageScore = averageScore.add(result.relevance);
-    });
 
-    return averageScore.div(BATCH_SIZE);
+    const results = await Promise.all(evaluationPromises);
+
+    // Calculate the sum of each column
+    const columnSums: Decimal[] = [];
+    for (let j = 0; j < results[0].length; j++) {
+      let sum = new Decimal(0);
+      for (let i = 0; i < results.length; i++) {
+        sum = sum.plus(results[i][j]);
+      }
+      columnSums.push(sum);
+    }
+
+    // Return the average of each column
+    return columnSums.map((sum) => sum.dividedBy(results.length));
   }
 
-  _generatePrompt(issue: string, comments: string) {
+  _generatePrompt(issue: string, comments: string[]) {
     if (!issue?.length) {
       throw new Error("Issue specification comment is missing or empty");
     }
-    return `I need to evaluate the relevance of GitHub contributors' comments to a specific issue specification. Specifically, I'm interested in how much the comment helps to further define the issue specification or contributes new information or research relevant to the issue. Please provide a float between 0 and 1 to represent the degree of relevance. A score of 1 indicates that the comment is entirely relevant and adds significant value to the issue, whereas a score of 0 indicates no relevance or added value.\n\nIssue Specification:\n\`\`\`\n${issue}\n\`\`\`\n\nConversation:\n\`\`\`\n${comments}\n\`\`\`\n\n\nTo what degree is of the comment in the conversation relevant and valuable to further defining the issue specification? Please reply ONLY with a float number between 0 and 1. The float should represent the degree of relevance and added value of the comment to the issue.`;
+    return `I need to evaluate the relevance of GitHub contributors' comments to a specific issue specification. Specifically, I'm interested in how much each comment helps to further define the issue specification or contributes new information or research relevant to the issue. Please provide a float between 0 and 1 to represent the degree of relevance. A score of 1 indicates that the comment is entirely relevant and adds significant value to the issue, whereas a score of 0 indicates no relevance or added value. Each contributor's comment is on a new line.\n\nIssue Specification:\n\`\`\`\n${issue}\n\`\`\`\n\nConversation:\n\`\`\`\n${comments
+      .map((comment) => comment)
+      .join(
+        "\n"
+      )}\n\`\`\`\n\n\nTo what degree are each of the comments in the conversation relevant and valuable to further defining the issue specification? Please reply with ONLY an array of float numbers between 0 and 1, corresponding to each comment in the order they appear. Each float should represent the degree of relevance and added value of the comment to the issue. The total length of the array in your response should equal exactly ${
+      comments.length
+    } elements.`;
   }
 }
