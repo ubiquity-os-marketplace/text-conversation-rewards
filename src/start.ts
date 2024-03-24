c214587180a587fb53fd3fba17f9cc22ede6ffac
gentlementlegen <FernandVEYRIER@users.noreply.github.com>
2024-03-25 08:55:21 +0900

    chore: fixed wrong length check on path elements

--- src/start.ts
+++ src/start.ts
@@ -118,7 +118,7 @@ export async function getAllTimelineEvents(issueParams: IssueParams): Promise<Gi
 
 export function parseGitHubUrl(url: string): { owner: string; repo: string; issue_number: number } {
   const path = new URL(url).pathname.split("/");
-  if (path.length < 4) {
+  if (path.length < 5) {
     throw new Error(`[parseGitHubUrl] Invalid url: [${url}]`);
   }
   return {
