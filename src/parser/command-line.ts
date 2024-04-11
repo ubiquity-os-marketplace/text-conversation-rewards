b8e81cf1c7a7a460c69240261d7cb1da48fc2158
gentlementlegen <fernand.veyrier@epitech.eu>
2024-04-11 14:25:51 +0900

    chore: changed arguments to be inside the CLI

--- src/parser/command-line.ts
+++ src/parser/command-line.ts
@@ -9,10 +9,16 @@ if (process.env.NODE_ENV === "test") {
   process.argv.splice(2);
   process.argv.push("-i");
   process.argv.push(`${process.env.TEST_ISSUE_URL}`);
+  process.argv.push("-n");
+  process.argv.push("100");
+  process.argv.push("-e");
+  process.argv.push("privateKey");
 }
 
 const program = new Command()
   .requiredOption("-i, --issue <url>", "The url of the issue to parse")
+  .requiredOption("-n, --evmNetworkId <number>", "The network ID", parseInt)
+  .requiredOption("-e, --evmPrivateEncrypted <key>", "The EVM private encrypted key")
   .option("-c, --config <path>", "The path to the desired configuration to use", "rewards-configuration.default.yml")
   .option("-f, --file <file>", "The target file to store the results in")
   .version(packageJson.version)
