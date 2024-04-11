59fdf04eebbfbc2104f07ee7f3b96a945eed84c5
Korrrba <gitcoiner@gmail.com>
2024-04-11 11:45:27 +0200

    chore: upgrade Knip to 5.7.0, fix all Knip warnings

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["src/index.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/data-collection/examples/*.ts"],
+  ignore: ["src/data-collection/examples/*.ts", "src/configuration/common-config-type.ts"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: [],
   jest: {
