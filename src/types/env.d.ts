392c334f6b501c230a6896fe9f395c2eb5fb8a8d
gentlementlegen <fernand.veyrier@epitech.eu>
2024-04-11 14:39:14 +0900

    chore: removed unused env.d.ts definitions

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -6,8 +6,6 @@ declare global {
       X25519_PRIVATE_KEY: string;
       SUPABASE_ANON_KEY: string;
       SUPABASE_URL: string;
-      NFT_CONTRACT_ADDRESS: string;
-      NFT_MINTER_PRIVATE_KEY: string;
     }
   }
 }
