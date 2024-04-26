2afc414e33b39df2197fef73c469cb106179b426
gentlementlegen <fernand.veyrier@epitech.eu>
2024-04-26 12:47:40 +0900

    chore: event payload

--- src/configuration/constants.ts
+++ src/configuration/constants.ts
@@ -3,3 +3,5 @@ import { config } from "dotenv";
 config();
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+console.log("GITHUB_TOKEN", GITHUB_TOKEN);
