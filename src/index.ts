2c15e7c44ea878221cce0afba4b93ffa3f4da067
gentlementlegen <fernand.veyrier@epitech.eu>
2024-05-30 14:27:03 +0900

    fix: skip on issue close not completed status

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,35 @@ import { IssueActivity } from "./issue-activity";
 import program from "./parser/command-line";
 import { Processor } from "./parser/processor";
 import { parseGitHubUrl } from "./start";
+import { getOctokitInstance } from "./get-authentication-token.ts";
 
 async function main() {
-  if (program.eventName === "issues.closed") {
-    const issue = parseGitHubUrl(program.eventPayload.issue.html_url);
+  const { eventPayload, eventName } = program;
+  if (eventName === "issues.closed") {
+    if (eventPayload.issue.state_reason !== "completed") {
+      const result = "# Issue was not closed as completed. Skipping.";
+      try {
+        await getOctokitInstance().issues.createComment({
+          body: `\`\`\`${result}\`\`\``,
+          repo: eventPayload.repository.name,
+          owner: eventPayload.repository.owner.login,
+          issue_number: eventPayload.issue.number,
+        });
+      } catch (e) {
+        console.error(e);
+      }
+      return result;
+    }
+    const issue = parseGitHubUrl(eventPayload.issue.html_url);
     const activity = new IssueActivity(issue);
     await activity.init();
     const processor = new Processor();
     await processor.run(activity);
     return processor.dump();
   } else {
-    console.warn(`${program.eventName} is not supported, skipping.`);
+    const result = `${eventName} is not supported, skipping.`;
+    console.warn(result);
+    return result;
   }
 }
 
