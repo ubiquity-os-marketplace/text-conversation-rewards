ca6e472511cbecad9a7b3ce7ba137e9c6b3ce3ff
gentlementlegen <fernand.veyrier@epitech.eu>
2024-06-11 12:55:10 +0900

    fix: updated test suite to match the new schema, fixed the async test to not run after tear down

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as core from "@actions/core";
 import { run } from "./run";
 
-run()
+export default run()
   .then((result) => {
     core?.setOutput("result", result);
+    return result;
   })
   .catch((e) => {
     console.error("Failed to run comment evaluation:", e);
     core?.setFailed(e.toString());
+    return e;
   });
