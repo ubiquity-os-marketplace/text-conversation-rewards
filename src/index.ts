f833afab5f508d3bc19df64432b46c4262116b98
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-07-16 14:54:28 +0900

    chore: debug context

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 import logger from "./helpers/logger";
 import { run } from "./run";
 
 export default run()
   .then((result) => {
+    console.log(JSON.stringify(github.context, null, 2));
     core?.setOutput("result", result);
     return result;
   })
