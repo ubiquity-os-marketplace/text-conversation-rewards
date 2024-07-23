d53060f7562b4e3cd9daa2ec8be2d352c034c1c4
whilefoo <whilefoo@users.noreply.github.com>
2024-07-23 12:18:19 +0200

    feat: retry configuration

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,6 +1,7 @@
 import { Octokit } from "@octokit/rest";
 import { retry } from "@octokit/plugin-retry";
 import program from "./parser/command-line";
+import configuration from "./configuration/config-reader";
 
 const customOctokit = Octokit.plugin(retry);
 
@@ -8,7 +9,13 @@ let octokitInstance: Octokit | null = null;
 
 function getOctokitInstance(): Octokit {
   if (!octokitInstance) {
-    octokitInstance = new customOctokit({ auth: program.authToken });
+    octokitInstance = new customOctokit({
+      auth: program.authToken,
+      retry: {
+        retries: configuration.dataCollection.maxAttempts,
+        retryAfterBaseValue: configuration.dataCollection.delayMs,
+      },
+    });
   }
   return octokitInstance;
 }
