fc291ef94e844cfa31fe7e9176fd318f03af8eb9
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-08-20 10:51:00 +0900

    chore: test changing top_p

--- src/parser/content-evaluator-module.ts
+++ src/parser/content-evaluator-module.ts
@@ -136,7 +136,8 @@ export class ContentEvaluatorModule implements Module {
         },
       ],
       max_tokens: maxTokens,
-      top_p: 0.5,
+      top_p: 1,
+      temperature: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
