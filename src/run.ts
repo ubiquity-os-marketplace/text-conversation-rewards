d640681cbcbbf5961e61c4f41eec8b82d963444d
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-10 09:59:12 +0900

    fix: add log message for closed pull requests
    Include log message indicating all linked pull requests are closed.

--- src/run.ts
+++ src/run.ts
@@ -22,6 +22,7 @@ export async function run() {
       await githubCommentModuleInstance.postComment(result.logMessage.diff);
       return result.logMessage.raw;
     }
+    console.log("All linked pull requests are closed. Generating rewards...");
     await githubCommentModuleInstance.postComment(logger.ok("Evaluating results. Please wait...").logMessage.diff);
     const issue = parseGitHubUrl(eventPayload.issue.html_url);
     const activity = new IssueActivity(issue);
