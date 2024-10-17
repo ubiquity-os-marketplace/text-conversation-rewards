b3546807cbeab948420a51456b5c32b7824102fd
jaykayudo <jaykayudo@gmail.com>
2024-10-17 03:49:53 +0100

    chore: add fee to github comment prompt column

---
+++ src/helpers/fee.ts
@@ -0,0 +1,6 @@
+import Decimal from "decimal.js";
+
+export function generateFeeString(reward: number | Decimal, feeRate: number | Decimal) {
+  const originalReward = new Decimal(reward).div(new Decimal(feeRate));
+  return originalReward.minus(new Decimal(reward)).toFixed(2);
+}
