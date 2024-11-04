f5a628393629809e94a37f59dcfe9e9f413b9de6
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-04 14:49:09 +0900

    chore: update kernel dependency and refactor plugin import
    Updated @ubiquity-os/ubiquity-os-kernel in package.json and refactored import in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { createPlugin, Manifest } from "@ubiquity-os/ubiquity-os-kernel";
+import { createActionsPlugin } from "@ubiquity-os/ubiquity-os-kernel";
 import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import manifest from "../manifest.json";
 import { SupportedEvents } from "./parser/command-line";
 import { run } from "./run";
 import envConfigSchema, { EnvConfig } from "./types/env-type";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
 
-export default createPlugin<PluginSettings, EnvConfig, SupportedEvents>(
+export default createActionsPlugin<PluginSettings, EnvConfig, SupportedEvents>(
   (context) => {
     return run(context);
   },
-  manifest as Manifest,
   {
     logLevel: (process.env.LOG_LEVEL as LogLevel) ?? "info",
+    // @ts-expect-error the schema is valid
     settingsSchema: pluginSettingsSchema,
+    // @ts-expect-error the schema is valid
     envSchema: envConfigSchema,
     ...(process.env.KERNEL_PUBLIC_KEY && { kernelPublicKey: process.env.KERNEL_PUBLIC_KEY }),
     postCommentOnError: true,
