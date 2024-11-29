87e517290be1df3a794a2d5370d198351f5b4f81
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-11-29 21:26:07 +0900

    Update src/configuration/data-purge-config.ts
    Co-authored-by: アレクサンダー.eth <4975670+0x4007@users.noreply.github.com>

--- src/configuration/data-purge-config.ts
+++ src/configuration/data-purge-config.ts
@@ -3,7 +3,7 @@ import { Type, Static } from "@sinclair/typebox";
 export const dataPurgeConfigurationType = Type.Object({
   skipCommentsWhileAssigned: Type.Boolean({
     default: true,
-    description: "Skip comments posted by a user while they are assigned to the issue",
+    description: "Do not offer credit to comments posted by a user between the time they were first assigned and last unassigned from the task.",
   }),
 });
 
