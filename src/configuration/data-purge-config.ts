733d803f0e15f3f497d0d4a859f5a0eb07e77e31
gentlementlegen <fernand.veyrier@epitech.eu>
2024-11-29 19:40:06 +0900

    chore: changed description for skipCommentsWhileAssigned

--- src/configuration/data-purge-config.ts
+++ src/configuration/data-purge-config.ts
@@ -3,7 +3,7 @@ import { Type, Static } from "@sinclair/typebox";
 export const dataPurgeConfigurationType = Type.Object({
   skipCommentsWhileAssigned: Type.Boolean({
     default: true,
-    description: "Skip comments posted by a user while it is assigned to the issue",
+    description: "Skip comments posted by a user while they are assigned to the issue",
   }),
 });
 
