a5aaece54176aa3e8ffd9faaa9f809517f0716c7
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-19 07:56:28 +0900

    test: fixing tests

--- tests/process.issue.test.ts
+++ tests/process.issue.test.ts
@@ -370,7 +370,7 @@ describe("Modules tests", () => {
         total: 9.25,
       },
       whilefoo: {
-        total: 9.25,
+        total: 9.084,
       },
     });
     if (oldLabels && activity.self) {
@@ -425,13 +425,13 @@ describe("Modules tests", () => {
     expect(result).toBe(9.25);
     const total = await processor.run(activity);
     expect(total).toMatchObject({
-      gentlementlegen: { total: 800, task: { multiplier: 1, reward: 400 }, userId: 9807008 },
+      gentlementlegen: { total: 18.5, task: { multiplier: 1, reward: 9.25 }, userId: 9807008 },
       user2: { total: 0, userId: 1 },
       "0x4007": {
-        total: 400,
+        total: 9.25,
       },
       whilefoo: {
-        total: 43.562,
+        total: 9.084,
       },
     });
   });
