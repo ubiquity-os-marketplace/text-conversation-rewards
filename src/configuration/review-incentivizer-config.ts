a2093edcb0b0a309db7198b2e1a535eed61bac45
ishowvel <rakshit.c16@gmail.com>
2024-12-28 20:38:57 +0530

    fix: add a description to the new config

--- src/configuration/review-incentivizer-config.ts
+++ src/configuration/review-incentivizer-config.ts
@@ -2,8 +2,20 @@ import { Static, Type } from "@sinclair/typebox";
 
 export const reviewIncentivizerConfigurationType = Type.Object(
   {
-    baseRate: Type.Number(),
-    conclusiveReviewCredit: Type.Number(),
+    /**
+     * Number of lines of code that equals $1 in review credit
+     */
+    baseRate: Type.Number({
+      default: 100,
+      description: "Number of lines of code that equals $1 in review credit",
+    }),
+    /**
+     * Flat rate bonus in dollars for completing a conclusive review i.e (Approved or Changes Requested)
+     */
+    conclusiveReviewCredit: Type.Number({
+      default: 25,
+      description: "Flat rate bonus in dollars for completing a conclusive review i.e (Approved or Changes Requested)",
+    }),
   },
   { default: {} }
 );
