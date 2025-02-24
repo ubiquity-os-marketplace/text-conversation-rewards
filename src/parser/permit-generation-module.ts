6bc2e246ded63d8286267ea96d5cc5e8bce768c6
zugdev <zug.dev@proton.me>
2025-02-24 18:08:56 -0300

    feat: fix error throwing in permit module

--- src/parser/permit-generation-module.ts
+++ src/parser/permit-generation-module.ts
@@ -277,7 +277,7 @@ export class PermitGenerationModule extends BaseModule {
       locationId = locationData.id;
     }
     if (!locationId) {
-      throw this.context.logger.error("Failed to retrieve the related location from issue", { issue }).logMessage.raw;
+      throw this.context.logger.error("Failed to retrieve the related location from issue", { issue });
     }
     return locationId;
   }
@@ -314,7 +314,7 @@ export class PermitGenerationModule extends BaseModule {
       throw this.context.logger.error("Failed to retrieve the related token from permit", {
         address,
         network,
-      }).logMessage.raw;
+      });
     }
 
     return tokenId;
@@ -344,7 +344,7 @@ export class PermitGenerationModule extends BaseModule {
       walletId = walletData.id;
     }
     if (!walletId) {
-      throw this.context.logger.error("Failed to retrieve the related wallet from permit", { address }).logMessage.raw;
+      throw this.context.logger.error("Failed to retrieve the related wallet from permit", { address });
     }
 
     const { data: partnerData } = await this._supabase.from("partners").select("id").eq("wallet_id", walletId).single();
@@ -367,7 +367,7 @@ export class PermitGenerationModule extends BaseModule {
       partnerId = partnerData.id;
     }
     if (!partnerId) {
-      throw this.context.logger.error("Failed to retrieve the related partner from permit", { address }).logMessage.raw;
+      throw this.context.logger.error("Failed to retrieve the related partner from permit", { address });
     }
 
     return partnerId;
