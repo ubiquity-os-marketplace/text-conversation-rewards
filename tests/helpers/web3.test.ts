ae7d79df1c55a57626117113115fde62ef312a10
gentlementlegen <fernand.veyrier@epitech.eu>
2025-03-26 11:32:30 +0900

    test: fixed web test

--- tests/helpers/web3.test.ts
+++ tests/helpers/web3.test.ts
@@ -3,10 +3,10 @@ import { getErc20TokenSymbol } from "../../src/helpers/web3";
 describe("web3.ts", () => {
   describe("getERC20TokenSymbol()", () => {
     it("Should return ERC20 token symbol", async () => {
-      const networkId = 100; // gnosis
-      const tokenAddress = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"; // WXDAI
-      const tokenSymbol = await getErc20TokenSymbol(networkId, tokenAddress);
-      expect(tokenSymbol).toEqual("WXDAI");
+      // const networkId = 100; // gnosis
+      // const tokenAddress = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"; // WXDAI
+      const tokenSymbol = await getErc20TokenSymbol();
+      expect(tokenSymbol).toEqual("XP");
     }, 120000);
   });
 });
