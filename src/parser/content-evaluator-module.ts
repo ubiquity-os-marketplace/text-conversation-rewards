06cec80adc1410e443f1a00116fbd16e4f801f04
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-30 13:52:47 +0900

    chore: added back deleted comments

--- src/parser/content-evaluator-module.ts
+++ src/parser/content-evaluator-module.ts
@@ -172,7 +172,7 @@ export class ContentEvaluatorModule extends BaseModule {
     );
 
     for (const currentComment of commentsWithScore) {
-      let currentRelevance = 1;
+      let currentRelevance = 1; // For comments not in fixed relevance types and missed by OpenAI evaluation
       if (this._fixedRelevances[currentComment.commentType]) {
         currentRelevance = this._fixedRelevances[currentComment.commentType];
       } else if (!isNaN(relevancesByAi[currentComment.id])) {
