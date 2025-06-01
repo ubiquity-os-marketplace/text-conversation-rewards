3c3686835c306ef0ed51530247d812f6f6e8db82
gentlementlegen <fernand.veyrier@epitech.eu>
2025-06-01 12:49:34 +0900

    chore: external content relevance module

---
+++ src/helpers/content/preprocess-external-content.ts
@@ -0,0 +1,88 @@
+import { GithubCommentScore } from "../../types/results";
+import MarkdownIt from "markdown-it";
+import { JSDOM } from "jsdom";
+import OpenAI from "openai";
+
+export class ExternalContentProcessor {
+  private readonly _md = new MarkdownIt();
+
+  constructor(
+    private readonly _openAi: OpenAI,
+    private readonly _llmModel: string
+  ) {}
+
+  public async evaluateExternalElements(comment: GithubCommentScore) {
+    const html = this._md.render(comment.content.replaceAll("\r", "\n"));
+    const jsDom = new JSDOM(html);
+
+    if (jsDom.window.document.body) {
+      const htmlElement = jsDom.window.document.body;
+      await this._handleAnchorElements(htmlElement);
+      await this._handleImageElements(htmlElement);
+    } else {
+      throw new Error(`Could not create DOM for comment [${JSON.stringify(comment)}]`);
+    }
+  }
+
+  private async _handleAnchorElements(htmlElement: HTMLElement) {
+    const anchors = htmlElement.getElementsByTagName("a");
+    for (const anchor of anchors) {
+      const href = anchor.getAttribute("href");
+      if (!href) continue;
+      const linkResponse = await fetch(href);
+      const contentType = linkResponse.headers.get("content-type");
+      if (!contentType || !contentType.startsWith("text/")) continue;
+      const linkContent = await linkResponse.text();
+      const llmResponse = await this._openAi.chat.completions.create({
+        model: this._llmModel,
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant that evaluates external content and summerizes it.",
+          },
+          {
+            role: "user",
+            content: `Summarize the following external content in one paragraph, written in a single line: ${linkContent}`,
+          },
+        ],
+      });
+      const altContent = llmResponse.choices[0]?.message?.content;
+      if (!altContent) continue;
+      // Should replace the content in the comment
+      console.log(altContent);
+    }
+  }
+
+  private async _handleImageElements(htmlElement: HTMLElement) {
+    const images = htmlElement.getElementsByTagName("img");
+    for (const image of images) {
+      const href = image.getAttribute("href");
+      if (!href) continue;
+      const linkResponse = await fetch(href);
+      const contentType = linkResponse.headers.get("content-type");
+      if (!contentType || !contentType.startsWith("image/")) continue;
+      const imageData = await linkResponse.arrayBuffer();
+      const base64Image = Buffer.from(imageData).toString("base64");
+      const llmResponse = await this._openAi.chat.completions.create({
+        model: this._llmModel,
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant that evaluates external images and summerizes it.",
+          },
+          {
+            role: "user",
+            content: [
+              { type: "text", text: "Describe this image concisely in one paragraph, written in a single line." },
+              { type: "image_url", image_url: { url: `data:image/jpeg;base64,${base64Image}` } },
+            ],
+          },
+        ],
+      });
+      const imageContent = llmResponse.choices[0]?.message?.content;
+      if (!imageContent) continue;
+      // Should replace the content in the image
+      console.log(imageContent);
+    }
+  }
+}
