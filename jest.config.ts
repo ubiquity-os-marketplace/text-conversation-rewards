32e05e51412355af18c3537744caf85899665b34
gentlementlegen <fernand.veyrier@epitech.eu>
2025-12-30 17:19:38 +0900

    test: remove jest-setup file from test configuration
    Cleaned up the setupFilesAfterEnv array by removing jest-setup.ts.

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ const cfg: Config = {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
-  setupFilesAfterEnv: ["dotenv/config", "<rootDir>/jest-setup.ts"],
+  setupFilesAfterEnv: ["dotenv/config"],
 };
 
 export default cfg;
